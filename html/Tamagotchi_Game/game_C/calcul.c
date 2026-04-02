c52cd525a2479f4acd524814b71bc2dcd3f7fd97
Anthony97480 <tony.ferrere.h@gmail.com>
2026-04-02 13:47:52 +0200

    test webAssembly

---
+++ html/Tamagotchi_Game/game_C/calcul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "calcul.h"
+
+int calcul(char Opp[3], int val1, int val2){
+    if(!strcmp(Opp, "MUL")){
+        return val1 * val2;
+    } else if(!strcmp(Opp, "ADD")){
+        return val1 + val2;
+    } else if(!strcmp(Opp, "SOU")){
+        return val1 - val2;
+    } else if(!strcmp(Opp, "DIV")){
+        if(val2 != 0){
+            return val1 / val2;
+        } else {
+            return 0;
+        }
+    } else{
+        return 0;
+    }
+}
---
+++ html/Tamagotchi_Game/game_C/calcul.h
@@ -0,0 +1,6 @@
+#ifndef __calcul__js__
+#define __calcul__js__
+
+int calcul(char Opp[3], int val1, int val2);
+
+#endif
