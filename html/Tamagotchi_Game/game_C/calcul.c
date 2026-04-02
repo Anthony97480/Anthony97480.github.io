b0277e3afcd302f3bd0bc2217cc8061cb8645939
Anthony97480 <tony.ferrere.h@gmail.com>
2026-04-02 14:10:33 +0200

    update

--- html/Tamagotchi_Game/game_C/calcul.c
+++ html/Tamagotchi_Game/game_C/calcul.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "calcul.h"
 
-int calcul(char Opp[3], int val1, int val2){
+int calcul(const char* Opp, int val1, int val2){
     if(!strcmp(Opp, "MUL")){
         return val1 * val2;
     } else if(!strcmp(Opp, "ADD")){
--- html/Tamagotchi_Game/game_C/calcul.h
+++ html/Tamagotchi_Game/game_C/calcul.h
@@ -1,6 +1,6 @@
 #ifndef __calcul__js__
 #define __calcul__js__
 
-int calcul(char Opp[3], int val1, int val2);
+int calcul(const char* Opp, int val1, int val2);
 
 #endif
