69f49d2a193af022f442f9036e8c6a2e959a9688
AnthonyFH <ferrere-hoar@insa-toulouse.fr>
2026-04-03 07:22:23 +0200

    test web Assembly

---
+++ docs/html/Tamagotchi_Game/game_C/tableau.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "tableau.h"
+
+typedef struct MyTable{
+    const char* Name;
+    struct MyTable* next;
+} MyTable;
+
+
+MyTable* NameTable = NULL;
+int index_NameTable = 0;
+
+void add_element(char* val){
+    MyTable* nextElement = (MyTable*) malloc(sizeof(MyTable));
+    nextElement->Name = strdup(val);
+    nextElement->next = NULL;
+    if(NameTable == NULL){
+        NameTable = nextElement;
+    } else{
+        MyTable* tmp = NameTable->next;
+        while (tmp != NULL)
+        {
+            tmp = tmp->next;
+        }
+        tmp = nextElement;
+    }
+    index_NameTable++;
+}
+
+char* get_element(int idx){
+    MyTable* tmp = NameTable;
+    int tmp_idx = 0;
+
+    while(tmp != NULL && tmp_idx != idx){
+        tmp = tmp->next;
+        tmp_idx++;
+    }
+
+    if(tmp != NULL){
+        return tmp->Name;
+    } else{
+        printf("erreur, élément rechercher non existant");
+        exit(1);
+    }
+}
---
+++ docs/html/Tamagotchi_Game/game_C/tableau.h
@@ -0,0 +1,8 @@
+#ifndef __table__js__
+#define __table__js__
+
+void add_element(char* val);
+
+char* get_element(int idx);
+
+#endif
